6f963d5415922b38a6f5352818797307116565cd
Steven Jennison <stvnjennison@gmail.com>
2018-12-25 17:04:23 -0500

    Update plugin.rb
    Update to change "description" to show ID instead of email

--- plugin.rb
+++ plugin.rb
@@ -84,6 +84,12 @@ class DiscordAuthenticator < ::Auth::OAuth2Authenticator
     return if user.user_avatar.try(:custom_upload_id).present?
     Jobs.enqueue(:download_avatar_from_url, url: avatar_url, user_id: user.id, override_gravatar: false)
   end
+    
+    
+  def description_for_user(user)
+    info = Oauth2UserInfo.find_by(user_id: user.id, provider: @name)
+    info&.uid || ""
+  end
 end
 
 auth_provider :title => 'with Discord',
